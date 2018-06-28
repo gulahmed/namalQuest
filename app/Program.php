18bbab46aa7f580e616370b561f7033bbf3dad5b
gulahmed <gulaftab@gmail.com>
2018-06-29 02:06:40 +0500

    fillable property set

--- app/Program.php
+++ app/Program.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Program extends Model
 {
     protected $table = 'applicant_program_details';
+    protected $guarded =[];
 
     public function user()
     {
