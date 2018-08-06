0248fb47fd67ed2b68f29b037075ad9be241c9ef
gulahmed <gulaftab@gmail.com>
2018-08-06 12:57:49 +0500

    Label change NAT-1 to NAT

--- resources/views/applicant/partials/_programs_form.blade.php
+++ resources/views/applicant/partials/_programs_form.blade.php
@@ -66,14 +66,14 @@
                         <div class="card bg-dark text-white">
                           <div class="card-body">
                           &#9638 &nbsp;For BSc Electrical Engineering \ BSc Computer Science, only combined Entry Test 2018 coducted by UET Lahore is accepted.<br>
-                          &#9638 &nbsp;For BBA, either NAT-I 2018 (conducted by NTS) <strong>OR</strong> Combined Entry Test 2018 (conducted by UET Lahore) is accepted.<br>
+                          &#9638 &nbsp;For BBA, either NAT 2018 (conducted by NTS) <strong>OR</strong> Combined Entry Test 2018 (conducted by UET Lahore) is accepted.<br>
                           </div>
                         </div>
 
                         <div class="form-group">
                           <select class="form-control select2" name="test_name_punjab" required @if (!empty($domicile->domicile_province) && $domicile->domicile_province != 'Punjab') disabled @endif style="width: 100%" id="dompunjab" >
                             <option disabled selected hidden>Choose ...</option>
-                            <option value="NAT-I" {{old('test_name', $test_name) =="NAT-I" ? 'selected': ''}}  >NAT-I 2018(NTS) (For BBA Only) (100 Marks)</option>
+                            <option value="NAT-I" {{old('test_name', $test_name) =="NAT-I" ? 'selected': ''}}  >NAT 2018(NTS) (For BBA Only) (100 Marks)</option>
                             <option value="UET Lahore" {{old('test_name', $test_name) =="UET Lahore" ? 'selected': ''}} >UET Lahore Combined Entry Test 2018 (400 Marks)</option>
                           </select>
                         </div>
@@ -83,7 +83,7 @@
                         <div class="card bg-dark text-white">
                           <div class="card-body">
                           For BSc Electrical Engineering \ BSc Computer Science \ BBA, following tests are accepted:<br>
-                          &#9638 &nbsp;NAT-I 2018 (conducted by NTS)<br> &emsp;<strong>OR</strong><br>
+                          &#9638 &nbsp;NAT 2018 (conducted by NTS)<br> &emsp;<strong>OR</strong><br>
                           &#9638 &nbsp;Test conducted by any public sector Engineering University<br>
                           </div>
                         </div>
@@ -91,7 +91,7 @@
                         <div class="form-group">
                           <select class="form-control select2" name="test_name_non_punjab" style="width: 100%" id="domnonpunjab" required @if (!empty($domicile->domicile_province)&& $domicile->domicile_province == 'Punjab') disabled @endif  onchange="nonpunjabtester()">
                             <option disabled selected hidden>Choose ...</option>
-                            <option value="NAT-I" {{old('test_name', $test_name) =="NAT-I" ? 'selected': ''}} >NAT-I 2018 (NTS) (100 Marks)</option>
+                            <option value="NAT-I" {{old('test_name', $test_name) =="NAT-I" ? 'selected': ''}} >NAT 2018 (NTS) (100 Marks)</option>
                             <option value="UET Lahore" {{old('test_name', $test_name) =="UET Lahore" ? 'selected': ''}} >UET Lahore Combined Entry Test 2018 (400 Marks)</option>
                             <option value="UET Peshawar" {{old('test_name', $test_name) =="UET Peshawar" ? 'selected': ''}}>UET Peshawar Test 2018 (800 Marks)</option>
                             <option value="NED Karachi" {{old('test_name', $test_name) =="NED Karachi" ? 'selected': ''}} >NED UET Karachi Test 2018 (100 Marks)</option>
@@ -103,17 +103,6 @@
                       </div>
                     </div>
 
-                    <div class="row">
-                      <div class="col-md-6"></div>
-                      <div class="col-md-6">
-                        <label for="othertestname">Other Test</label>
-                        <div class="form-group">
-                          <div class="input-group">
-                            <input type="text" class="form-control" id="othertestname" disabled placeholder="Enter test name ...">
-                          </div>
-                        </div>
-                      </div>
-                    </div>
 
 
 		  <div class="form-row">
